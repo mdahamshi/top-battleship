8e10a61a10991c0c5ff4e19086e74bd6b7f63876
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-07-08 20:17:08 +0300

    class: gameboard class implements

--- src/class/Gameboard.js
+++ src/class/Gameboard.js
@@ -1,3 +1,91 @@
+import { Ship } from "./Ship.js";
+import { generateID, createBoard } from "@sarawebs/sb-utils";
+import { Cell } from "./Cell.js";
 export class Gameboard {
-  
+  #boardSize = 10;
+  #ships = [];
+  #missed = [];
+  #hit = [];
+  #board = [];
+  #id;
+  constructor() {
+    this.#board = createBoard(this.#boardSize, this.#boardSize, (boardRow) =>
+      boardRow.push(new Cell()),
+    );
+
+    this.#id = generateID("board");
+  }
+  getID() {
+    return this.#id;
+  }
+  getCordinate(row, col) {
+    return this.#board[row][col];
+  }
+  getCell(row, col) {
+    return this.#board[row][col];
+  }
+  insertShip(ship, row, col, direction = "H") {
+    if (!(ship instanceof Ship) || ! this.isValid([row, col])) return null;
+    let directionCapital = direction.toUpperCase();
+    const length = ship.length;
+    const makeEndCoordinate = {
+      H: ([r, c]) => {
+        let cordinate = [];
+        let endCordinate = [r, c + length - 1];
+        if (!this.isValid(endCordinate)) return null;
+
+        for (let i = col; i < col + length; i++) {
+          if (!this.isEmpty(row, i)) return null;
+          cordinate.push([row, i]);
+        }
+        return cordinate;
+      },
+      V: ([r, c]) => {
+        let cordinate = [];
+        let endCordinate = [r + length - 1, c];
+        if (!this.isValid(endCordinate)) return null;
+
+        for (let i = row; i < row + length; i++) {
+          if (!this.isEmpty(i, col)) return null;
+          cordinate.push([i, col]);
+        }
+        return cordinate;
+      },
+    };
+
+    const cordinates = makeEndCoordinate[directionCapital]([row, col]);
+    if(! cordinates)
+      return null;
+    cordinates.forEach(([r, c]) => this.getCell(r, c).setValue(ship));
+    return ship;
+  }
+  isEmpty(row, col) {
+    return this.getCell(row, col).isEmpty();
+  }
+  isValid(cordinate) {
+    return cordinate[0] < this.#boardSize && cordinate[1] < this.#boardSize;
+  }
+  receiveAttack(row, col) {
+    const cell = this.getCell(row, col);
+    if (cell.isShip()) {
+      cell.getValue().hit();
+      cell.setHit();
+      this.#hit.push([row, col]);
+      return;
+    }
+    if (!cell.isEmpty()) return;
+
+    cell.setMissed();
+    this.#missed.push([row, col]);
+  }
+  allShipsSunk() {
+    return this.#ships.every((ship) => ship.isSunk());
+  }
+  getBoard() {
+    return this.#board;
+  }
+  printBoard = () => {
+    const boardWithCellValues = this.#board.map((row) => row.map((cell) => cell.getValue()));
+    console.table(boardWithCellValues);
+  };
 }
--- src/class/Gameboard.test.js
+++ src/class/Gameboard.test.js
@@ -1,14 +1,36 @@
-import {Gameboard} from './Gameboard.js'
+import { Gameboard } from "./Gameboard.js";
+import { Ship } from "./Ship.js";
 
 const board = new Gameboard();
 describe("Testing Gameboard class", () => {
-  test("hit method", () => {
-    expect(ship.hit()).toEqual(1);
+  test("getCordinate method", () => {
+    expect(board.getCell(0, 0).isEmpty()).toBeTruthy();
   });
-  test("isSumk method", () => {
-    expect(ship.isSunk()).toEqual(false);
+  test("insertShip method", () => {
+    let ship = new Ship(4);
+    let ship2 = new Ship(1);
+    board.insertShip(ship, 0, 0, "V");
+    expect(board.insertShip(ship, 0, 0, 'v')).toBeNull();
+    expect(board.insertShip(ship2, 9, 0, 'v')).toBeTruthy();
+    expect(board.insertShip(ship2, 10, 0, 'v')).toBeNull();
+    expect(board.insertShip(ship2, 0, 0, 'v')).toBeNull();
+    expect(board.getCell(0, 0).isShip()).toBeTruthy();
   });
-  test("length method", () => {
-    expect(ship.length()).toEqual(4);
+  test("receiveAttack method", () => {
+    board.receiveAttack(0, 1);
+    const ship = board.getCell(0, 0).getValue();
+    [0, 1, 2, 3].forEach((r) => board.receiveAttack(r, 0));
+    expect(board.getCell(0, 1).isMissed()).toBeTruthy();
+    expect(board.getCell(0, 0).isHit()).toBeTruthy();
+    expect(ship.isSunk()).toBeTruthy();
   });
+  test("getID method", () => {
+    expect(board.getID()).toMatch(/board/);
+  });
+  test("board instanceof", () => {
+    expect(board instanceof Gameboard).toBeTruthy();
+  });
+});
+afterAll(() => {
+  board.printBoard();
 });
